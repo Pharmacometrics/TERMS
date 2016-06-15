248dd425689de082c485cce1af90f8509b06b807
KAWASHIMA Yusuke <kawashima-y@phs.osaka-u.ac.jp>
2016-06-15 19:26:34 +0900

    initial commit

---
+++ headfoot.js
@@ -0,0 +1,17 @@
+function header() {
+document.write ("
+<a href=index.html></a>　
+<a href=books.html></a>　
+<a href=kabucal.html></a>　
+<a href=kabucal2.html></a>　
+<a href=anomaly.html></a>　
+<a href=shmtg.html></a>　
+<a href=stance.html></a>　
+
+");
+}
+
+function footer() {
+document.write("Copyright (C) 2009- TERMS第三者委員会, All rights reserved. <br>");
+document.write("last modified : " + document.lastModified );
+}
